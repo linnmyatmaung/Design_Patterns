ddf651122bb9472c83b539e8387fe2c77e313b15
linnmyatmaung <linnmyatmaung03@gmail.com>
2025-04-28 20:47:36 +0630

    Design Patterns

---
+++ Builder.php
@@ -0,0 +1,48 @@
+<?php
+ class ProfileBuilder
+ {
+    private $name;
+    private $phone;
+    public function setName($name)
+    {
+        $this->name = $name;
+        return $this;
+    }
+    public function setPhone($phone)
+    {
+        $this->phone = $phone;
+        return $this;
+    }
+    public function getName()
+    {
+        return $this->name;
+    }
+
+ public function getPhone()
+    {
+      return $this->phone;
+    }
+ function build()
+ {
+   return new Profile($this);    
+}
+ }
+ class Profile
+ {
+ public $name;
+ public $phone;
+ public function __construct(ProfileBuilder $pb)
+    {
+ $this->name = $pb->getName();
+ $this->phone = $pb->getPhone();
+    }
+ static function builder()
+  { 
+     return new ProfileBuilder();
+  }
+ $user = Profile::builder()
+ ->setName("Alice")
+ ->setPhone("321456")
+ ->build();
+ var_dump($user);
+ // object(Profile){ ["name"]=> "Alice" ["phone"]=> "321456" }
